e138d7ac5aa678b696921d08394a1a9f0361ef31
Yves Kleny <yvesk.martins@gmail.com>
2026-06-25 21:37:36 -0300

    criando rotas `POST` para `/aluno` e `/tarefa`

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,7 +8,13 @@ alunos = [
     {"id": 3, "nome": "Carla", "curso": "Técnico em Informática"}
 ]
 
-@app.route('/')
+tarefas = [
+    {"id": 1,"titulo": "Tarefa 1" , "descricao": "Fazer a tarefa 1", "concluida": False},
+    {"id": 2,"titulo": "Tarefa 2" , "descricao": "Fazer a tarefa 2", "concluida": True},
+    {"id": 3,"titulo": "Tarefa 3" , "descricao": "Fazer a tarefa 3", "concluida": False}
+]
+
+@app.route('/', methods=['GET'])
 def home():
     return jsonify({
         "message": "Esta mensagem veio do alem!",
@@ -16,7 +22,7 @@ def home():
     })
 
 
-@app.route('/health')
+@app.route('/health', methods=['GET'])
 def health():
     return jsonify({
         "status": "ok",
@@ -24,19 +30,76 @@ def health():
     })
 
 
-@app.route('/aluno')
+@app.route('/aluno', methods=['GET'])
 def listar_alunos():
     return jsonify(alunos)
 
 
-@app.route('/aluno/<int:id>')
+@app.route('/aluno/<int:id>', methods=['GET'])
 def buscar_aluno(id):
     for aluno in alunos:
         if aluno["id"] == id:
             return jsonify(aluno)
-    return jsonify({"erro": "Aluno não encontrado"}), 404
+    return jsonify({"Erro": "Aluno não encontrado"}), 404
+
+@app.route("/aluno", methods=["POST"])
+def criar_aluno():
+    dados = request.get_json()
+
+    if not dados:
+        return jsonify({"Erro": "Dados inválidos!"}), 400
+    
+    if "nome" not in dados or "curso" not in dados:
+        return jsonify({"Erro": "Campos obrigatórios não preenchidos!"}), 400
+    
+    novo_aluno = {
+        "id": len(alunos) + 1,
+        "nome": dados["nome"],
+        "curso": dados["curso"]
+    }
+
+    alunos.append(novo_aluno)
+
+    return jsonify(novo_aluno), 201
+
+
+@app.route("/tarefa", methods=["GET"])
+def listar_tarefas():
+    return jsonify(tarefas)
+
+
+@app.route("/tarefa/<int:id>", methods=["GET"])
+def buscar_tarefa(id):
+    for tarefa in tarefas:
+        if tarefa["id"] == id:
+            return jsonify(tarefa)
+
+    return jsonify({"Erro": "Tarefa não encotrada!"}), 404
+
+
+@app.route("/tarefa", methods=["POST"])
+def criar_tarefa():
+    dados = request.get_json()
+
+    if not dados:
+        return jsonify({"Erro": "Dados inválidos!"}), 400
+    
+    if "titulo" not in dados or "descricao" not in dados:
+        return jsonify({"Erro": "Campos obrigatórios não preenchidos!"}), 400
+    
+
+    nova_tarefa = {
+        "id": len(tarefas) + 1,
+        "titulo": dados["titulo"],
+        "descricao": dados["descricao"],
+        "concluida": False
+    }
+    tarefas.append(nova_tarefa)
+    return jsonify(nova_tarefa), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
